57a0c0c3b91bfc2910a730e4d5af42d1b7526ad3
AlainGZ <devalgz955@gmail.com>
2025-09-02 13:36:58 -0500

    feat(infraestructura): Crear documentacion con swagger

--- domain/usecase/src/main/java/co/com/crediya/usecase/usuario/UsuarioConstantes.java
+++ domain/usecase/src/main/java/co/com/crediya/usecase/usuario/UsuarioConstantes.java
@@ -10,8 +10,25 @@ public final class UsuarioConstantes {
 	public static final String MENSAJE_CORREO_INVALIDO = "Formato de correo electrónico inválido";
 	public static final String MENSAJE_SALARIO_INVALIDO = "El salario base debe estar entre 0 y 15,000,000";
 	public static final String MENSAJE_CORREO_REPETIDO = "El correo ya esta registrado";
+
 	public static final String ERROR_INTERNO = "Error Interno";
-	public static final String VALIDAR_DATOS = "Validacion de Datos";
+	public static final String VALIDAR_DATOS = "Error de Validacion de Datos";
+	public static final String OK = "Usuario registrado exitosamente";
+
+	public static final String TITULO = "CrediYa - Registro de Usuarios";
+	public static final String VERSION = "1.0";
+	public static final String DESCRIPCION = "Documentación de la API de Usuarios con WebFlux y Clean Architecture";
+	public static final String FUNCION_DESCRITA = "Permite registrar un usuario nuevo";
+
+	public static final String ERROR_400 = "400";
+	public static final String ERROR_409 = "409";
+	public static final String ERROR_200 = "200";
+	public static final String ERROR_500 = "500";
+
+	public static final String ROUTER_PATH = "/api/v1/usuarios";
+	public static final String ROUTER_BEANMETHOD = "listenSaveUsuario";
+	public static final String ROUTER_OPERATIONID = "saveUsuario";
+
 
 	public static final long SALARIO_MIN = 0L;
 	public static final long SALARIO_MAX = 15_000_000L;
--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/RouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/RouterRest.java
@@ -1,7 +1,16 @@
 package co.com.crediya.api;
 
 import co.com.crediya.api.config.UsuarioPath;
+import co.com.crediya.model.usuario.Usuario;
+import co.com.crediya.usecase.usuario.UsuarioConstantes;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.RouterOperation;
+import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -19,6 +28,28 @@ public class RouterRest {
     private final Handler usuarioHandler;
 
     @Bean
+    @RouterOperations({
+            @RouterOperation(
+                    path = UsuarioConstantes.ROUTER_PATH,
+                    beanClass = Handler.class,
+                    beanMethod = UsuarioConstantes.ROUTER_BEANMETHOD,
+                    operation = @Operation(
+                            operationId = UsuarioConstantes.ROUTER_OPERATIONID,
+                            summary = UsuarioConstantes.FUNCION_DESCRITA,
+                            requestBody = @RequestBody(
+                                    required = true,
+                                    content = @Content(schema = @Schema(implementation = Usuario.class))
+                            ),
+                            responses = {
+                                    @ApiResponse(responseCode = UsuarioConstantes.ERROR_200, description = UsuarioConstantes.OK,
+                                            content = @Content(schema = @Schema(implementation = Usuario.class))),
+                                    @ApiResponse(responseCode = UsuarioConstantes.ERROR_400, description = UsuarioConstantes.VALIDAR_DATOS),
+                                    @ApiResponse(responseCode = UsuarioConstantes.ERROR_409, description = UsuarioConstantes.MENSAJE_CORREO_REPETIDO),
+                                    @ApiResponse(responseCode = UsuarioConstantes.ERROR_500, description = UsuarioConstantes.ERROR_INTERNO)
+                            }
+                    )
+            )
+    })
     public RouterFunction<ServerResponse> routerFunction(Handler handler) {
        return route(POST(usuarioPath.getUsuarios()), usuarioHandler::listenSaveUsuario);
            }
